105e7493a1dbe8f00db68d0283d9abc566606541
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2017-03-16 14:31:36 +0200

    add more detailed logback config

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,11 +1,16 @@
+def logPattern = "%d{HH:mm:ss.SSS} [%thread] %highlight(%-5level) %logger{40}: %msg%n%ex{full, sun, java.lang, java.util}"
+
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{40}: %msg%n"
+        pattern = logPattern
     }
 }
 
 root(INFO, ["STDOUT"])
 
 logger("jnaalisv", INFO)
-logger("org.hibernate", INFO)
+logger("org.hibernate", TRACE)
+//logger("org.hibernate.SQL", DEBUG)
+//logger("org.hibernate.type.descriptor.sql.BasicBinder", TRACE)
+//logger("org.hibernate.tool.hbm2ddl.SchemaExport", DEBUG)
 
