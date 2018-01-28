a27db469733c4213935d05ff5843b2f361223425
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-01-28 23:05:24 +0200

    use Dockerized PostgreSQL instead of H2DB

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-#topics:
-- hibernate 
-- jpa
-- transactions
 
+Start the database:
+```
+$ sudo docker-compose -f stack.yml up
+```
+
+Connect to the database:
+```
+psql -h localhost -p 5432 -U postgres
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,14 @@ repositories {
 }
 
 val hibernateVersion by extra {"5.2.12.Final"}
+val postgreSqlVersion by extra {"42.2.1"}
 val hikariCpVersion by extra {"2.7.6"}
 
 dependencies {
+
+    runtime ("org.postgresql:postgresql:$postgreSqlVersion")
     runtime ("org.codehaus.groovy:groovy:2.4.9")
     runtime ("ch.qos.logback:logback-classic:1.2.3")
-    runtime ("com.h2database:h2:1.4.194")
 
     compile ("org.slf4j:slf4j-api:1.7.25")
     compile ("org.hibernate:hibernate-core:$hibernateVersion")
