17f57e2f21e2206f5db159f6b0aad995dc217357
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-01-29 14:25:39 +0200

    refactor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,14 +12,14 @@ val hikariCpVersion by extra {"2.7.6"}
 
 dependencies {
 
-    runtime ("org.postgresql:postgresql:$postgreSqlVersion")
-    runtime ("ch.qos.logback:logback-classic:1.2.3")
-
     compile ("org.slf4j:slf4j-api:1.7.25")
     compile ("org.hibernate:hibernate-core:$hibernateVersion")
-    compile ("org.hibernate:hibernate-hikaricp:$hibernateVersion")
-    compile ("com.zaxxer:HikariCP:$hikariCpVersion")
 
     testCompile ("junit:junit:4.12")
     testCompile ("org.assertj:assertj-core:3.9.0")
+
+    testRuntime ("org.postgresql:postgresql:$postgreSqlVersion")
+    testRuntime ("com.zaxxer:HikariCP:$hikariCpVersion")
+    testRuntime ("org.hibernate:hibernate-hikaricp:$hibernateVersion")
+    testRuntime ("ch.qos.logback:logback-classic:1.2.3")
 }
