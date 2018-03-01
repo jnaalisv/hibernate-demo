49e2742a6b9313ddc960d4247af022d14adeaa2e
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-03-01 11:03:28 +0200

    configure jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
 }
 
 repositories {
@@ -39,3 +40,11 @@ dependencies {
 val test: Test by tasks
 test.useJUnitPlatform()
 
+val jacocoTestReport: JacocoReport by tasks
+jacocoTestReport.reports {
+    xml.isEnabled = false
+    html.isEnabled = true
+    html.destination = file("${buildDir}/jacocoHtml")
+}
+
+test.finalizedBy(jacocoTestReport)
