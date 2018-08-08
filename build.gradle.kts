d0de236c2072980592ffaa213fc5a89ade1e2aa5
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-08-08 17:15:50 +0300

    jaxb should be runtime dependency instead of testRuntime

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,17 +23,17 @@ dependencies {
     runtime ("org.hibernate:hibernate-hikaricp:$hibernateVersion")
     runtime ("ch.qos.logback:logback-classic:1.2.3")
 
+    // JAXB (JSR 222) Standalone Implementation
+    runtime ("javax.xml.bind:jaxb-api:$jaxbVersion")
+    runtime ("com.sun.xml.bind:jaxb-impl:$jaxbVersion")
+    runtime ("com.sun.xml.bind:jaxb-core:$jaxbVersion")
+
+    // JSR-925 Java Beans Activation Framework
+    runtime ("javax.activation:activation:1.1.1")
+
     testCompile ("org.junit.jupiter:junit-jupiter-api:$JUnitVersion")
     testCompile ("org.junit.jupiter:junit-jupiter-params:$JUnitVersion")
     testRuntime ("org.junit.jupiter:junit-jupiter-engine:$JUnitVersion")
-
-    // Java EE modules used to be included with the JRE. They were deprecated in Java SE 9 with the
-    // declared intent to remove them in a future release.
-    testRuntime ("javax.xml.bind:jaxb-api:$jaxbVersion") // JAXB (JSR 222) Standalone Implementation
-    testRuntime ("com.sun.xml.bind:jaxb-impl:$jaxbVersion")
-    testRuntime ("com.sun.xml.bind:jaxb-core:$jaxbVersion")
-
-    testRuntime ("javax.activation:activation:1.1.1") // JSR-925 Java Beans Activation Framework
 }
 
 val jacocoTestReport: JacocoReport by tasks
