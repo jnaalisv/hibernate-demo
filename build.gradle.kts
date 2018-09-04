fcc1bbad0a4407a589242b587bb06666b7d4774a
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-09-04 09:34:19 +0300

    JUnit Jupiter 5.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 val hibernateVersion = "5.3.5.Final"
 val postgreSqlVersion = "42.2.5"
 val hikariCpVersion = "3.2.0"
-var JUnitVersion = "5.2.0"
+var JUnitVersion = "5.3.0"
 
 dependencies {
 
