737236ba23c98decb244f15d969758835fbe5bde
Juho Naalisvaara <jnaalisv@hal.local>
2018-12-28 13:28:30 +0200

    nicer way to configure jacoco and other tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,20 +29,21 @@ dependencies {
     testCompile ("org.junit.jupiter:junit-jupiter:$JUnitVersion")
 }
 
-val jacocoTestReport: JacocoReport by tasks
-jacocoTestReport.reports {
-    xml.isEnabled = false
-    html.isEnabled = true
-    html.destination = file("${buildDir}/jacocoHtml")
-}
-
 tasks {
-    "test" (Test::class) {
+    jacocoTestReport {
+        reports {
+            xml.isEnabled = false
+            csv.isEnabled = true
+            html.destination = file("${buildDir}/jacocoHtml")
+        }
+    }
+
+    test {
         useJUnitPlatform()
         finalizedBy(jacocoTestReport)
     }
 
-    "compileJava" (JavaCompile::class) {
+    compileJava {
         options.compilerArgs = listOf("-Xlint:all", "-Werror")
     }
 }
